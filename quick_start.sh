ff047b1c9b8a9753b65384c2bc8040c716daa998
James Stevenson <28961606+user1342@users.noreply.github.com>
2025-03-26 21:33:38 +0000

    Update quick_start.sh

--- quick_start.sh
+++ quick_start.sh
@@ -18,5 +18,5 @@ nohup folly-api "https://api.openai.com/v1" --model "gpt-4" --api-key "$CHATGPT_
 echo "Folly API now running with ChatGPT."
 
 # Start the folly UI
-folly-ui "http://localhost:8000"
+folly-ui "http://localhost" --port 8000
 
