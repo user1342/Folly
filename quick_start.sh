3f94b240bf828a9e210dd332148d13dd488fc75e
James Stevenson <28961606+user1342@users.noreply.github.com>
2025-03-26 21:31:24 +0000

    Update quick_start.sh

--- quick_start.sh
+++ quick_start.sh
@@ -15,7 +15,8 @@ git clone https://github.com/user1342/folly.git
 # Start the folly API using OpenAI's ChatGPT API
 nohup folly-api "https://api.openai.com/v1" --model "gpt-4" --api-key "$CHATGPT_API_KEY" folly/example_challenges/prompt_injection_masterclass.json > output.log 2>&1 & disown
 
-# Start the folly UI
-nohup folly-ui "http://localhost:8000" > folly-ui.log 2>&1 & disown
+echo "Folly API now running with ChatGPT."
 
-echo "Folly API and UI are now running with ChatGPT."
+# Start the folly UI
+folly-ui "http://localhost:8000"
+
